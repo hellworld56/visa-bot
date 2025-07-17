43f4617f8bdf7f92d3ee4336a8e02415088a45ac
hellworld56 <105684366+hellworld56@users.noreply.github.com>
2025-07-17 17:44:13 +0500

    Update final-bot.py

--- final-bot.py
+++ final-bot.py
@@ -39,7 +39,7 @@ def wait_for_available_slot_and_click(page):
 
 def login_and_book():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 
